a1b1d2681015241118b8e207b03f0de7557058c5
Ilona Mereuta <edoilo.3521@gmail.com>
2022-11-06 20:52:29 +0100

    es-10 - props-02 done

--- my-app-react/src/Welcome.js
+++ my-app-react/src/Welcome.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export class Welcome extends React.Component {
     render(){
-        return <p>Hello, {this.props.name}</p>
+        return <p>Hello, {this.props.name || 'User'}</p>
     }
 
 }
+
 
