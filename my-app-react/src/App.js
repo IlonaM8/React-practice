abb6eb92cdfb5516c8bb7ebdbb9a47d2206d097a
Ilona Mereuta <edoilo.3521@gmail.com>
2022-11-20 16:39:32 +0100

    props-04 corrected

--- my-app-react/src/App.js
+++ my-app-react/src/App.js
@@ -5,10 +5,13 @@ import { Welcome } from './Welcome';
 
 export class App extends React.Component{
     render(){
+        const strongTag = <strong>Sean</strong>
         return (
             <div>
                 < HelloWorld />
-                < Welcome name={[<strong>Sean</strong>]} age="200"/>
+                < Welcome name={<strong>Sean</strong>} age="200"/>
+                < Welcome component={<p>Ciao</p>} name={strongTag} age="200"/> 
+                
             </div>
         )
 
--- my-app-react/src/Welcome.js
+++ my-app-react/src/Welcome.js
@@ -5,8 +5,8 @@ export class Welcome extends React.Component {
     render(){
         return (
         <div>
-            <p>Hello, {this.props.name || 'User'}</p>
-            <p>Your age is: {this.props.age}</p>
+            <p>Hello, {this.props.name}</p>
+            {this.props.component}
             
         </div> 
         )
