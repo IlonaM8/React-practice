1afd37b732ba3ed5ff4aea9bf5bf67a62bce9a02
Ilona Mereuta <edoilo.3521@gmail.com>
2022-11-26 20:04:35 +0100

    function component-01 done

--- my-app-react/src/Welcome.js
+++ my-app-react/src/Welcome.js
@@ -2,24 +2,40 @@ import React from 'react'
 import { Age } from './Age'
 
 
+//the same component written as a function component:
 
-export class Welcome extends React.Component {
-    render(){
-        return (
-        <div className='welcome'>
-            {this.props.name === 'John' && this.props.age < 65 && this.props.age > 18 ? <p>Your age is: {this.props.age}</p> :  null}
-            
-            
-            <p>Hello, {this.props.name}</p>    
-            <Age age={16} />
-           
-        </div> 
-        )
-        
-    }
+export function Welcome(props){
+    return(
+        <div>
+            {props.name === 'John' && props.age < 65 && props.age > 18 ? <p>Your age is: {props.age}</p> :  null}
+            {props.name && <h4>Hello, {props.name}!</h4>}
+            {<Age age={26}/>}
 
+        </div>
+    )
 }
 
-//default Props
 
-Welcome.defaultProps = { name: "User"}
+
+
+
+
+// export class Welcome extends React.Component {
+//     render(){
+//         return (
+//         <div className='welcome'>
+//             {this.props.name === 'John' && this.props.age < 65 && this.props.age > 18 ? <p>Your age is: {this.props.age}</p> :  null}
+            
+            
+//             <p>Hello, {this.props.name}</p>    
+//             <Age age={16} />
+           
+//         </div> 
+//         )
+        
+//     }
+
+// }
+
+//default Props
+//Welcome.defaultProps = { name: "User"}
