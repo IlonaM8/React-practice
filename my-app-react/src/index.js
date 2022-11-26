284c35cc331587a7e37e928513428e61c6e12ca5
Ilona Mereuta <edoilo.3521@gmail.com>
2022-11-26 01:43:28 +0100

    tailwind does not work..

--- my-app-react/src/index.js
+++ my-app-react/src/index.js
@@ -1,9 +1,15 @@
-import ReactDom from 'react-dom'
+import React from 'react'
+// import ReactDom from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import { App } from './App'
 import './index.css' 
 
 
 
-const root = document.querySelector('#root');
+//const root = document.querySelector('#root');
+// ReactDom.render(<App />, root);
 
-ReactDom.render(<App />, root);
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
