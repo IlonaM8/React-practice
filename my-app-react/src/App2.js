ee006667cc343c17d3b6339c54c0c53c78851e63
Ilona Mereuta <edoilo.3521@gmail.com>
2022-12-01 16:46:32 +0100

    react router 02 done

--- my-app-react/src/App2.js
+++ my-app-react/src/App2.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Container } from './Container';
 import { Routes, Route }  from 'react-router-dom';
 import { Welcome } from './Welcome';
+import { Counter1 } from './Counter1';
+import { ClickCounter } from './ClickCounter';
 
 export function App2() {
   return (
@@ -10,6 +12,7 @@ export function App2() {
         <div>App2: Learning React Router </div>
         <Routes>
             <Route path="/" element={<Welcome name="John" />}/>
+            <Route path='/counter' element={<ClickCounter />} />
         </Routes>
         </Container>
        
